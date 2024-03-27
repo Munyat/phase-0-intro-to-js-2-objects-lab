80b86b3116d20409f697579d041cea93a70a2b7f
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2024-03-27 21:18:35 +0300

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+  name: "",
+  streetAddress: "",
+};
+function updateEmployeeWithKeyAndValue(employee, name, value) {
+  const newEmployee = { ...employee };
+  newEmployee[name] = value;
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, value) {
+  employee[name] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, name) {
+  const deleteProperty = { ...employee };
+  delete deleteProperty[name];
+  return deleteProperty;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+  delete employee[name];
+  return employee;
+}
